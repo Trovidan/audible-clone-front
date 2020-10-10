5258d0ed3c9ccc26cbe3ee0dcb863400076ceac2
Trovidan <pranav07092k@gmail.com>
2020-10-10 17:03:28 +0530

    Added buld

---
+++ build/precache-manifest.5d3cff779d3e330daf4cb66965a38e01.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9940ba5f802d099c940de23fc6cb4046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb53b22811d60ab2db7",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ba481cd499234d066464",
+    "url": "/static/css/main.69edaf2a.chunk.css"
+  },
+  {
+    "revision": "1cb53b22811d60ab2db7",
+    "url": "/static/js/2.668cc991.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "/static/js/2.668cc991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba481cd499234d066464",
+    "url": "/static/js/main.bb472ff5.chunk.js"
+  },
+  {
+    "revision": "c02785a0eef1b6e414b8",
+    "url": "/static/js/runtime-main.617e8564.js"
+  },
+  {
+    "revision": "d4901de39bf7b7f1fe1b84dcc468b786",
+    "url": "/static/media/audible_logo.d4901de3.svg"
+  },
+  {
+    "revision": "f2a7dfcfe6f3c980d60ffb3f111fb0b8",
+    "url": "/static/media/bookLibrary.f2a7dfcf.jpg"
+  },
+  {
+    "revision": "62e51539378e4c52f646bc41099e0c09",
+    "url": "/static/media/left-arrow.62e51539.svg"
+  },
+  {
+    "revision": "ba838726c5e0c7f82d8a4cb5af7b1342",
+    "url": "/static/media/right-arrow.ba838726.svg"
+  }
+]);
