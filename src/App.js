2aaa41be3f8f20a9d45905ce7e43c29bb499060f
Pranav Mishra <pranav07092k@gmail.com>
2020-12-18 14:07:03 +0530

    lazy load components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, lazy, Suspense} from 'react';
 import { BrowserRouter, Route, Switch,Redirect} from 'react-router-dom';
 import universalCookie from 'universal-cookie';
-import Home from "./routes/home";
-import Login from "./routes/login";
-import ProfilePage from './routes/userProfile';
-import axios from './components/axios';
-import CreateUser from './routes/createUser';
-import Explore from './routes/explore';
-import Wishlist from './routes/wishlist';
-import Library from './routes/library';
-import Cart from './routes/cart';
-import Navbar from './components/navbar.js';
-import Footer from './components/footer.js';
-import {LoginProvider} from './components/loginStatus';
-import BookPage from './routes/bookPage';
-import Verification from './routes/verification';
+import { LoginProvider } from "./components/loginStatus.js"
+import logo from './components/images/svgIcons/audible_logo.svg';
+import './App.css'
+
+const Home = lazy(() => import("./routes/home.js") );
+const Login = lazy(() => import("./routes/login.js") );
+const ProfilePage = lazy(() => import("./routes/userProfile.js") );
+const axios = lazy(() => import("./components/axios.js") );
+const CreateUser = lazy(() => import("./routes/createUser.js") );
+const Explore = lazy(() => import("./routes/explore.js") );
+const Wishlist = lazy(() => import("./routes/wishlist.js") );
+const Library = lazy(() => import("./routes/library.js") );
+const Cart = lazy(() => import("./routes/cart.js") );
+const Navbar = lazy(() => import("./components/navbar.js") );
+const Footer = lazy(() => import("./components/footer.js") );
+const BookPage = lazy(() => import("./routes/bookPage.js") );
+const Verification = lazy(() => import("./routes/verification.js") );
 
 
 export default function App(){
@@ -245,26 +248,34 @@ export default function App(){
     removeFromWishlist: removeFromWishlist
   };
   return (
+    
     <LoginProvider value={providerValues}>
-      <BrowserRouter >
-        <Switch>
-          <ProtectedLogin exact path="/login" auth={auth} component={Login} />
-          <Route exact path="/verification/:token" auth={auth} component={Verification} />
-          <ProtectedLogin exact path="/createUser" auth={auth} component={CreateUser} />
-          <Route path="/">
-            <Navbar/>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/explore" component={Explore} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/book/:bookID" component={BookPage} />
-            <ProtectedRoute exact path="/profile" auth={auth} component={ProfilePage} />
-            <ProtectedRoute exact path="/wishlist" auth={auth} component={Wishlist} />
-            <ProtectedRoute exact path="/library" auth={auth} component={Library} />
-            <Footer/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Suspense fallback={
+            <div className="app-lazy-loading">
+              <img alt="audible loading" className="app-lazy-loading-image" src={logo} />
+            </div>
+          }
+      >
+        <BrowserRouter >
+          <Switch>
+            <ProtectedLogin exact path="/login" auth={auth} component={Login} />
+            <Route exact path="/verification/:token" auth={auth} component={Verification} />
+            <ProtectedLogin exact path="/createUser" auth={auth} component={CreateUser} />
+            <Route path="/">
+              <Navbar />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/explore" component={Explore} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/book/:bookID" component={BookPage} />
+              <ProtectedRoute exact path="/profile" auth={auth} component={ProfilePage} />
+              <ProtectedRoute exact path="/wishlist" auth={auth} component={Wishlist} />
+              <ProtectedRoute exact path="/library" auth={auth} component={Library} />
+              <Footer />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
     </LoginProvider>
   );
 }
